5e7c33046c3fafb8d9ecff0d9a26a6cf2a1265ae
Alanko Jenni <s2200726@BC-C02J61EAQ6LR-MacBook-Air.local>
2022-08-24 13:21:20 +0300

    more solution added

---
+++ week 3/Temperatures/script.js
@@ -0,0 +1,7 @@
+const convert = () =>  {
+
+let celsius = document.querySelector('celcius').value;
+let fahren = document.querySelector('fahren').value;
+let kelvin = document.querySelector('kelvin').value;
+    
+}
