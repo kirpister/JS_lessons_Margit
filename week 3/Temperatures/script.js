0f185aad4fb96b69c77bf6c389a1797b741e7612
Alanko Jenni <s2200726@BC-C02J61EAQ6LR-MacBook-Air.local>
2022-08-24 14:50:08 +0300

    added more

--- week 3/Temperatures/script.js
+++ week 3/Temperatures/script.js
@@ -1,7 +1,23 @@
-const convert = () =>  {
+const convert = (id, val) =>  {
 
-let celsius = document.querySelector('celcius').value;
-let fahren = document.querySelector('fahren').value;
-let kelvin = document.querySelector('kelvin').value;
+val = parseFloat(val);
+
+const inputCelsius = document.querySelector('inputCelcius');
+const inputFahren = document.querySelector('inputFahren');
+const inputKelvin = document.querySelector('inputKelvin');
+
+if (id == "inputFahren") {
+    inputCelsius.value = ((valNum - 32)) / 1.8.toFixed(2);
+    inputKelvin.value = (valNum + 273.15).toFixed(2);
+
+} if (id == "inputCelcius") {
+    inputFahren.value = (val * 1.8 + 32).toFixed(2);
+    inputKelvin.value = (val + 273.15).toFixed(2);
+
+} if (id == "inputKelvin") {
+    inputFahren.value = ((val - 273.15) * 1.8 + 32).toFixed(2);
+    inputCelsius.value = (val - 273.15).toFixed(2);
+}
+
     
 }
