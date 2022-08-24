53368e90a257a9490a1f224c841d6891b81c4ee0
Alanko Jenni <s2200726@BCC02J6AQ6LRAir.wlan>
2022-08-24 20:44:49 +0300

    fixes

--- week 3/Temperatures/script.js
+++ week 3/Temperatures/script.js
@@ -1,23 +1,21 @@
 const convert = (id, val) =>  {
-
 val = parseFloat(val);
 
-const inputCelsius = document.querySelector('inputCelcius');
-const inputFahren = document.querySelector('inputFahren');
-const inputKelvin = document.querySelector('inputKelvin');
+const inputFahren = document.querySelector('#inputFahren');
+const inputCelsius = document.querySelector('#inputCelcius');
+const inputKelvin = document.querySelector('#inputKelvin');
 
 if (id == "inputFahren") {
-    inputCelsius.value = ((valNum - 32)) / 1.8.toFixed(2);
-    inputKelvin.value = (valNum + 273.15).toFixed(2);
-
-} if (id == "inputCelcius") {
+    inputCelsius.value = ((val - 32) / 1.8).toFixed(2);
+    inputKelvin.value = ((val - 32) / 1.8 + 273.15).toFixed(2);
+} 
+if (id == "inputCelcius") {
     inputFahren.value = (val * 1.8 + 32).toFixed(2);
     inputKelvin.value = (val + 273.15).toFixed(2);
-
-} if (id == "inputKelvin") {
+} 
+if (id == "inputKelvin") {
     inputFahren.value = ((val - 273.15) * 1.8 + 32).toFixed(2);
     inputCelsius.value = (val - 273.15).toFixed(2);
 }
 
-    
-}
+};
