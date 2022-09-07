3ad6ef09a0aba7dbba01d0be0652e7036a393c86
kirpister <zirpister@gmail.com>
2022-09-07 12:13:46 +0300

    new tasks

---
+++ week 4/Insurance/script.js
@@ -0,0 +1,78 @@
+const form = document.querySelector('form');
+const customer = document.querySelector('#customerName');
+const customerName = document.getElementById('customerName');
+const age = document.querySelector('#customerAge');
+const customerHealth = document.querySelectorAll('[name="health"]');
+const customerHabits = document.querySelectorAll('[name="habits"]');
+
+const getQuote = (event) => {
+
+    event.preventDefault();
+    
+    let customerName = customer.value;
+    let customerAge = age.value;
+    let healthResult = [];
+    let habitsResult = [];
+    let quote = 0;
+    let coef = 0;
+    //let result = '';
+
+    
+    if (customerAge < 18) {
+        coef = 0;
+    }
+    else if (customerAge <= 25) {
+        coef = 0.1;
+    }
+    else if (customerAge <= 35) {
+        coef = 0.3;
+    }
+    else if (customerAge <= 45) {
+        coef = 0.6;
+    }
+    else if (customerAge <= 55) {
+        coef = 1;
+    }
+    else if (customerAge <= 65) {
+        coef = 1.5;
+    }
+    else { 
+        coef = 2.1;
+    }
+
+    
+
+customerHealth.forEach((item) => {
+    if (item.checked) {
+        healthResult.push(item.value);
+        coef = coef + 0.01;
+    }
+});
+
+
+
+customerHabits.forEach((item) => {
+    if (item.checked) {
+        habitsResult.push(item.value);
+    
+        if (item.value == 'daily exercise'){
+            coef = coef - 0.05;
+        }
+        else {
+            coef = coef + 0.05;
+        }
+
+    }
+});
+
+ console.log(habitsResult);
+ console.log(coef);
+
+quote = 500 + coef * 500;
+
+document.querySelector('#quote').textContent = `Hello ${customerName}.  Your risk score is ${quote.toFixed(2)}.`;
+
+form.reset();
+};
+
+form.addEventListener('submit', getQuote);
---
+++ week 5/gradient/script.js
@@ -0,0 +1,62 @@
+const color1 = document.querySelector("#color1");
+const color2 = document.querySelector("#color2");
+let body = document.getElementById("gradient");
+let getCss = document.querySelector("h4");
+
+const getGradient = (event) => {
+    
+    body.style.backgroundImage = "linear-gradient(to top, " 
+    + color1.value 
+    + ", "
+    + color2.value
+    + ")";
+}
+
+color1.addEventListener("input", getGradient);
+color2.addEventListener("input", getGradient);
+
+const setDirection = (event) => {
+    
+    
+let direction = document.querySelector('input[name="direction"]:checked').value
+
+if (direction === "down-right") {
+    direction = body.style.backgroundImage = "linear-gradient(to bottom right, " +color1.value+ ", "+ color2.value + ")";
+    }
+
+else if (direction === "down") {
+    direction = body.style.backgroundImage = "linear-gradient(to bottom , " +color1.value+ ", "+ color2.value + ")";
+    }
+  
+else if (direction === "down-left") {
+    direction = body.style.backgroundImage = "linear-gradient(to bottom left , " +color1.value+ ", "+ color2.value + ")";
+    }
+    
+else if (direction === "right") {
+    direction = body.style.backgroundImage = "linear-gradient(to right , " +color1.value+ ", "+ color2.value + ")";
+    }
+   
+ else if (direction === "left") {
+    direction = body.style.backgroundImage = "linear-gradient(to left , " +color1.value+ ", "+ color2.value + ")";
+    }
+
+else if (direction === "up-right") {
+    direction = body.style.backgroundImage = "linear-gradient(to top right , " +color1.value+ ", "+ color2.value + ")";
+    }
+
+else if (direction === "up") {
+    direction = body.style.backgroundImage = "linear-gradient(to top , " +color1.value+ ", "+ color2.value + ")";
+    }
+
+else if (direction === "up-left") {
+    direction = body.style.backgroundImage = "linear-gradient(to top left , " +color1.value+ ", "+ color2.value + ")";
+    }
+      console.log(direction);
+
+    getCss.textContent = body.style.backgroundImage + ";"; 
+    console.log(getCss);
+} 
+
+color1.addEventListener("input", setDirection);
+color2.addEventListener("input", setDirection);
+
